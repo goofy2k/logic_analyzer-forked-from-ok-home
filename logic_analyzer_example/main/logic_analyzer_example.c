dde6c5ad0366646ad4ac62f16da020a4f6ac31a9
Goofy2k <26250702+goofy2k@users.noreply.github.com>
2026-04-19 22:58:23 +0200

    .

--- logic_analyzer_example/main/logic_analyzer_example.c
+++ logic_analyzer_example/main/logic_analyzer_example.c
@@ -1,4 +1,4 @@
-
+﻿
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
@@ -16,16 +16,22 @@
 void app_main(void)
 {
     extern void test_sample_init();
+    printf("BREADCRUMB 1\n");
     // extern void test_air();
     test_sample_init();
     // test_air();
     vTaskDelay(5);
+    printf("BREADCRUMB 1a\n");
 #ifdef CONFIG_ANALYZER_USE_SUMP 
+    printf("BREADCRUMB 1b\n");
 #ifdef CONFIG_IDF_TARGET_ESP32
+    printf("BREADCRUMB 1c\n");
     esp_log_level_set("*", ESP_LOG_NONE); // sump default run on port0 - disable log
 #endif
+    printf("BREADCRUMB 2\n");
     logic_analyzer_sump();
 #endif
+    printf("BREADCRUMB 2a\n");
 #ifdef CONFIG_ANALYZER_USE_WS
     logic_analyzer_ws_server();
 #endif
@@ -36,6 +42,7 @@ void app_main(void)
 #endif
     logic_analyzer_cli();
 #endif
+    printf("BREADCRUMB 3\n");
     while (1)
     {
         vTaskDelay(100);
